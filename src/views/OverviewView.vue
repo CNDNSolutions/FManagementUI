e4cd2d2f72e0491c9b35c7d47cf7385190546597
AnvilM <211anvil112@gmail.com>
2024-07-13 11:10:37 +0300

    add: OverviewView view

---
+++ src/views/OverviewView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import CalendarComponent from "@/components/CalendarComponent.vue";
+import ProfitComponent from "@/components/Profit/ProfitComponent.vue";
+import MainChartComponent from "@/components/Profit/MainChartComponent.vue";
+import TitlePathComponent from "@/components/TitlePathComponent.vue";
+import ProfitListComponent from "@/components/Profit/ProfitListComponent.vue";
+import axios from "axios";
+import moment from "moment";
+</script>
+
+<template>
+    <div class="w-5/6 @container" v-if="monthData && yearData && lastMonthData">
+        <TitlePathComponent />
+        <div class="w-full flex flex-col [&>*+*]:mt-4 bg-red-500">aboba</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            monthData: false,
+            lastMonthData: false,
+            yearData: false,
+        };
+    },
+    mounted() {
+        let data = JSON.parse(localStorage.getItem("data"));
+
+        this.monthData = data.month;
+        this.lastMonthData = data.lastMonth;
+        this.yearData = data.year;
+    },
+    methods: {},
+};
+</script>
