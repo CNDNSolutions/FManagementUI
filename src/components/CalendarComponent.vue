41045e8d428d4ece19d95def64ee8f57bdf173be
AnvilM <211anvil112@gmail.com>
2024-07-14 05:59:20 +0300

    fix: calendar event

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <VDatePicker v-model.range="this.date" :is-dark="true" @update:modelValue="this.$emit('dateUpdated')">
+        <VDatePicker v-model.range="this.date" :is-dark="true" @popover-will-hide="this.$emit('dateUpdated')">
             <template #default="{ togglePopover }">
                 <div class="flex justify-center items-center px-3 text-lg rounded max-h-9 h-9 min-h-9 hover:bg-primary/80 cursor-pointer active:bg-primary/70 bg-primary/90" @click="togglePopover()">{{ moment(this.date.start).format("YYYY MMMM DD") }} - {{ moment(this.date.end).format("YYYY MMMM DD") }}</div>
             </template>
