5a585427adb412d4cd2b63439d305263f6236c6e
AnvilM <211anvil112@gmail.com>
2024-07-14 06:18:51 +0300

    add: Time helper methods

--- src/Helpers/Time.js
+++ src/Helpers/Time.js
@@ -1,11 +1,19 @@
 import moment from "moment";
 
 export const startOfMonth = () => {
-    return toFormat(moment().startOf("month").format("YYYY-MM-DD HH:mm:ss"));
+    return toFormat(moment().startOf("month"));
 };
 
 export const endOfMonth = () => {
     return toFormat(moment().endOf("month").startOf("day"));
+};
+
+export const startOfYear = () => {
+    return toFormat(moment().startOf("year"));
+};
+
+export const endOfYear = () => {
+    return toFormat(moment().endOf("year").startOf("day"));
 };
 
 export const toFormat = (date) => {
