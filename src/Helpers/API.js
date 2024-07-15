deffe2b21b8c6607b533cbdbb55fa144619d2411
AnvilM <211anvil112@gmail.com>
2024-07-15 06:07:15 +0300

    add: default headers to request

--- src/Helpers/API.js
+++ src/Helpers/API.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import moment from "moment";
 import { toFormat } from "./Time";
+import { DEFAULT_HEADERS, ENTRIES_URL } from "@/Config/API";
 
 export const byPeriod = (start, end) => {
     return new Promise((resolve, reject) => {
         axios
-            .get("http://localhost:8000/api/Entries?periodStart=" + toFormat(start) + "&periodEnd=" + toFormat(end))
+            .get(ENTRIES_URL + "?periodStart=" + toFormat(start) + "&periodEnd=" + toFormat(end), { headers: DEFAULT_HEADERS })
             .then((response) => {
                 resolve(response.data);
             })
